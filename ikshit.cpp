8672a9325eda3c2c6433cf97969fbe41910dcc49
ikshitjalan <jalanikshit1@gmail.com>
2019-09-08 14:54:26 +0530

    fourth commit

--- ikshit.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{   
-    cout << "Hi i am ikshit jalan and i am mad for ibneet kaur"<<endl;
-    
-    return 0;
-}
---
+++ revise.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+typedef struct  Node{
+
+    int data;
+    Node* next;
+}Node;
+
+Node* newNode(int newData){
+    Node* node = new Node();
+    node->data = newData;
+    node->next = NULL;
+    return node;
+}
+
+void push(Node** node,int newData){
+    Node* newnode = newNode( newData);
+    newnode->next = (*node)->next;
+    (*node) = newnode;
+
+}
+
+void insert(Node** node,int newData,int num){
+    Node* newnode = newNode(newData);
+    Node* temp = (*node);
+    int i = 1;
+    while(i!=num){
+        temp = temp->next;
+        i++;
+    }
+    newnode->next = temp->next;
+
+}
+
+void append(Node** node,int newData){
+    Node* newnode = newNode(newData);
+    Node* temp = (*node);
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    newnode->next = temp->next;
+
+}
+void printList(Node *node)  
+{  
+    while (node != NULL)  
+    {  
+        cout<<" "<<node->data;  
+        node = node->next;  
+    }  
+}  
+
+int main(){
+    Node* head = NULL;  
+      
+    // Insert 6. So linked list becomes 6->NULL  
+    append(&head, 6);  
+      
+    // Insert 7 at the beginning.  
+    // So linked list becomes 7->6->NULL  
+    push(&head, 7);  
+      
+    // Insert 1 at the beginning.  
+    // So linked list becomes 1->7->6->NULL  
+    push(&head, 1);  
+      
+    // Insert 4 at the end. So  
+    // linked list becomes 1->7->6->4->NULL  
+    append(&head, 4);  
+      
+    // Insert 8, after 7. So linked  
+    // list becomes 1->7->8->6->4->NULL  
+    insert(&head,3, 2);  
+      
+    cout<<"Created Linked list is: ";  
+    printList(head);  
+      
+    return 0;  
+}
